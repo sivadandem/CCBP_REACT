771d97111b511b48e8b37e7d631189af278d4221
sivadandem <sivadandem@gmail.com>
2025-11-04 10:50:10 +0530

    componentlifecyclefolder

---
+++ Componentlifecycle( time )/src/App.js
@@ -0,0 +1,33 @@
+import {Component} from 'react'
+import Clock from './components/Clock'
+
+import './App.css'
+
+class App extends Component {
+  state = {showClock: false}
+
+  onshowClock = () => {
+    this.setState(prevState => {
+      const {showClock} = prevState
+      return {showClock: !showClock}
+    })
+  }
+
+  render() {
+    const {showClock} = this.state
+    return (
+      <div className="app-container">
+        <button
+          type="button"
+          className="toggle-button"
+          onClick={this.onshowClock}
+        >
+          {showClock ? 'Show Clock' : 'Hide Clock'}
+        </button>
+        {showClock && <Clock />}
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ Componentlifecycle( time )/src/components/Clock/index.js
@@ -0,0 +1,28 @@
+import {Component} from 'react'
+import './index.css'
+
+class Clock extends Component {
+  state = {date: new Date()}
+
+  componentDidMount() {
+    this.timerId = setInterval(this.tick, 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId)
+  }
+
+  tick = () => {
+    this.setState({date: new Date()})
+  }
+
+  render() {
+    const {date} = this.state
+    return (
+      <div className="clock-container">
+        <h1 className="heading">{date.toLocaleTimeString()}</h1>
+      </div>
+    )
+  }
+}
+export default Clock
